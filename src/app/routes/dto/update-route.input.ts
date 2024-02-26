0f0fdd9dd25612060a6ecd86e610813d9ad117cb
Victor Ojong <victorojong78@gmail.com>
2024-02-26 11:26:03 +0100

    user-branch

--- src/app/routes/dto/update-route.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateRouteInput } from './create-route.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateRouteInput extends PartialType(CreateRouteInput) {
-  @Field(() => Int)
-  id: number;
-}
--- src/app/routes/routes.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RoutesService } from './routes.service';
-import { RoutesResolver } from './routes.resolver';
-
-@Module({
-  providers: [RoutesResolver, RoutesService],
-})
-export class RoutesModule {}
